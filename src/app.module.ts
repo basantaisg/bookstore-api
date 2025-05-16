7e600d0fb5129976b9713b42788577791243a884
Basanta <basantaparajuli.11@gmail.com>
2025-05-16 20:36:13 +0545

    installing related dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/graphql/schema.gql',
+      sortSchema: true,
+      graphiql: true,
+      playground: true,
+    }),
+  ],
 })
 export class AppModule {}
